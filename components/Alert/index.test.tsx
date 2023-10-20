9e052f63359043c7973ccae07eac90794e243372
Akos Komuves <akoskm@gmail.com>
2023-10-20 15:56:34 +0200

    add icon component and tests

---
+++ components/Alert/index.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Alert from './index';
+
+describe('Alert', () => {
+  it('should match snapshot', () => {
+    const { container } = render(<Alert />);
+    expect(container).toMatchSnapshot();
+  });
+});
+
+
+
+
+
---
+++ components/Alert/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Icon from '@/components/Icon';
+
+const Alert = () => {
+  return (
+    <div className="alert">
+      You write tests. Great job!
+      <Icon name="close" />
+    </div>
+  );
+};
+export default Alert;
+
+
+
